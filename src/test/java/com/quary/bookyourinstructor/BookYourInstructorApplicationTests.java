310c082b5e1925aea49fe439cb0703cfb1cad40f
Wojciech Góralczyk <goralczyk4@gmail.com>
2022-03-22 23:14:18 +0100

    Generated application

---
+++ src/test/java/com/quary/bookyourinstructor/BookYourInstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quary.bookyourinstructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourInstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
